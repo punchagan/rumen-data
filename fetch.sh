f946ad72203d303b9b0f609ab13cecfe56590984
Puneeth Chaganti <punchagan@muse-amuse.in>
2026-02-04 09:54:26 -0800

    Update: fetch script

--- fetch.sh
+++ fetch.sh
@@ -4,13 +4,22 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
+if [ -z "${GITHUB_ACTIONS:-}" ]; then
+    echo "This script can be set up to run periodically using cron. \
+        This may be useful, since running the fetch on GitHub Actions fails \
+        for some sites due to their anti-bot measures."
+    echo "To run this script as a cron job, add something like this to your crontab :"
+    echo "0 * * * * /path/to/fetch.sh >> /path/to/fetch.log 2>&1"
+    echo "This will run the script every hour and log output to fetch.log."
+fi
+
 # Update the local repository if not running in a GitHub Action
 if [ -z "${GITHUB_ACTIONS:-}" ]; then
     git pull --rebase origin
 fi
 
 # Check if readability-cli is installed
-command -v readablxe >/dev/null 2>&1 || {
+command -v readable >/dev/null 2>&1 || {
     echo "readability-cli is not installed. Please install it with:"
     echo "  npm install -g readability-cli"
     exit 1
